ea5608ec1ba85ff3dfa15047da9c2866fe490dc7
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-11 14:07:20 -0500

    disallowed closing of empty orders

--- pages/cart.js
+++ pages/cart.js
@@ -1,13 +1,13 @@
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
-import CardLayout from '../components/card-layout'
-import Layout from '../components/layout'
-import Navbar from '../components/navbar'
-import CartDetail from '../components/order/detail'
-import CompleteFormModal from '../components/order/form-modal'
-import { completeCurrentOrder, getCart } from '../data/orders'
-import { getPaymentTypes } from '../data/payment-types'
-import { removeProductFromOrder, removeAllProductsFromOrder } from '../data/products'
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+import CardLayout from "../components/card-layout"
+import Layout from "../components/layout"
+import Navbar from "../components/navbar"
+import CartDetail from "../components/order/detail"
+import CompleteFormModal from "../components/order/form-modal"
+import { completeCurrentOrder, getCart } from "../data/orders"
+import { getPaymentTypes } from "../data/payment-types"
+import { removeProductFromOrder, removeAllProductsFromOrder } from "../data/products"
 
 export default function Cart() {
   const [cart, setCart] = useState({})
@@ -34,14 +34,13 @@ export default function Cart() {
 
   const completeOrder = (paymentTypeId) => {
     const payment = { payment_type: paymentTypeId }
-    completeCurrentOrder(cart.id, payment)
-      .then(() => router.push("/my-orders"))
+    completeCurrentOrder(cart.id, payment).then(() => router.push("/my-orders"))
   }
 
   const removeProduct = (productId) => {
     removeProductFromOrder(productId).then(refresh)
   }
-  
+
   const removeAllProduct = () => {
     removeAllProductsFromOrder().then(refresh)
   }
@@ -54,13 +53,29 @@ export default function Cart() {
         paymentTypes={paymentTypes}
         completeOrder={completeOrder}
       />
-      <CardLayout title="Your Current Order">
-        <CartDetail cart={cart} removeProduct={removeProduct} />
-        <>
-          <a className="card-footer-item" onClick={() => setShowCompleteForm(true)}>Complete Order</a>
-          <a className="card-footer-item"onClick={() => removeAllProduct()}>Delete Order</a>
-        </>
-      </CardLayout>
+      {cart.products && cart.products.length > 0 ? (
+        <CardLayout title="Your Current Order">
+          <CartDetail cart={cart} removeProduct={removeProduct} />
+          <>
+            <a className="card-footer-item" onClick={() => setShowCompleteForm(true)}>
+              Complete Order
+            </a>
+            <a className="card-footer-item" onClick={() => removeAllProduct()}>
+              Delete Order
+            </a>
+          </>
+        </CardLayout>
+      ) : (
+        <div className="columns is-centered is-vcentered">
+          <div className="column is-6">
+            <div className="card">
+              <header className="card-header">
+                <h3 className="card-header-title">Your Current Order Is Empty</h3>
+              </header>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   )
 }
