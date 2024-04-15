352ad9d5f8341c26219fcdc1935a753032d23f2a
Hewson Schulz <hewsonschulz@gmail.com>
2024-04-15 15:01:17 -0500

    fixed store info on profile

--- components/store/card.js
+++ components/store/card.js
@@ -1,21 +1,21 @@
 import Link from "next/link"
 
-export function StoreCard({ store, width = "is-half", totalProducts }) {
+export function StoreCard({ favorite, width = "is-half", totalProducts }) {
   return (
     <div className={`column ${width}`}>
       <div className="card">
         <header className="card-header">
-          <p className="card-header-title">{store.name}</p>
+          <p className="card-header-title">{favorite.store.name}</p>
         </header>
         <div className="card-content">
           <p className="content">
-            Owner: {store.seller.first_name} {store.seller.last_name}
+            Owner: {favorite.seller.first_name} {favorite.seller.last_name}
           </p>
-          <div className="content">{store.description}</div>
-          <p>Total Products: {totalProducts}</p>
+          <div className="content">{favorite.store.description}</div>
+          {/*//TODO: <p>Total Products: {totalProducts}</p> */}
         </div>
         <footer className="card-footer">
-          <Link href={`stores/${store.id}`}>
+          <Link href={`stores/${favorite.store.id}`}>
             <a className="card-footer-item">View Store</a>
           </Link>
         </footer>
